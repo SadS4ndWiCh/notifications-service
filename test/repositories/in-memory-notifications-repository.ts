7419e0af7ccc4e265ca943298ae3bd1c31e9d34c
SadSAndWiCh _ <caiocamargo007@gmail.com>
2022-12-15 21:10:03 -0300

    test: add save, count and findMany in memory notification repository methods

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -6,7 +6,38 @@ export class InMemoryNotificationsRepository
 {
   public notifications: Notification[] = [];
 
+  async findById(notificationId: string): Promise<Notification | null> {
+    const notification = this.notifications.find(
+      (item) => item.id === notificationId,
+    );
+
+    if (!notification) return null;
+
+    return notification;
+  }
+
+  async findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+    return this.notifications.filter(
+      (item) => item.recipientId === recipientId,
+    );
+  }
+
+  async countManyByRecipientId(recipientId: string): Promise<number> {
+    return this.notifications.filter((item) => item.recipientId === recipientId)
+      .length;
+  }
+
   async create(notification: Notification): Promise<void> {
     this.notifications.push(notification);
+  }
+
+  async save(notification: Notification): Promise<void> {
+    const notificationIdx = this.notifications.findIndex(
+      (item) => item.id === notification.id,
+    );
+
+    if (notificationIdx >= 0) {
+      this.notifications[notificationIdx] = notification;
+    }
   }
 }
